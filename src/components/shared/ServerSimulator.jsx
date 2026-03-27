8163d971b89f802e8cf503a4b74beebd171fbd4e
swaraj <pawarswaraj605@gmail.com>
2026-03-27 10:52:08 +0530

    layer 2.3

--- src/components/shared/ServerSimulator.jsx
+++ src/components/shared/ServerSimulator.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { useStore } from '../store/useStore';
+import { useStore } from '../../store/useStore';
 
 const ServerSimulator = () => {
   const { incrementImpact, foodFeed, donors, set } = useStore();
